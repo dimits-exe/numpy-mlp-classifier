e35315558e03be8024354bde7048dc1d3e826371
Dimitris Tsirbas <manisfatsa@gmail.com>
2022-12-04 13:30:30 +0200

    Add main function

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,3 +1,4 @@
+import load_mnist
 import numpy as np
 
 
@@ -35,11 +36,11 @@ class LogisticRegClassifier:
         h = sigmoid(x.dot(theta))
         regularization = 1 / (2 * m) * lamda * np.sum(theta ** 2)
         # why did we apply a '-' here?
-        current_j = (1.0 / m) * ((-y).dot(np.log(h)) - (1 - y).T.dot(np.log(1 - h))) + regularization
-        y = y.reshape(y.shape[0], 1)
+        current_cost = (1.0 / m) * ((-y).dot(np.log(h)) - (1 - y).T.dot(np.log(1 - h))) + regularization
+        y = y.reshape(y.shape[0], 1)  # prevent numpy broadcast to 2d array
         regularization = lamda * theta / m  # why do we divide by m?
-        gradient = (1.0 / m) * x.T.dot(h - y) + regularization
-        return current_j, gradient
+        gradient = ((1.0 / m) * x.T.dot(h - y)) + regularization
+        return current_cost, gradient
 
     @classmethod
     def _gradient_ascent(cls, x: np.ndarray, y: np.ndarray, theta: np.ndarray, lamda: float, alpha: float, iters: int,
@@ -48,10 +49,10 @@ class LogisticRegClassifier:
 
         for i in range(iters):
             error, gradient = LogisticRegClassifier._cost_gradient(x, y, theta, lamda)
-            theta += alpha * gradient  # addition because we are ascending
+            theta -= alpha * gradient  # addition because we are ascending
 
-            if print_results:
-                print(error)
+            if print_results and i % 100 == 0:
+                print("Iteration ", i, " Error:", error)
 
             cost_history.append(error[0])
 
@@ -60,3 +61,21 @@ class LogisticRegClassifier:
 
 def sigmoid(x: np.ndarray) -> np.ndarray:
     return 1.0 / (1.0 + np.exp(x))
+
+
+def main():
+    classifier = LogisticRegClassifier(iters=500, alpha=0.01, lamda=0.1, print_history=True)
+    data = load_mnist.load_data()
+
+    cost_history = classifier.train(data.x_train, data.y_train)
+    print(cost_history)
+
+    labels, probabilities = classifier.predict(data.x_test)
+    true_predictions = np.count_nonzero(np.where(labels == 0, 5, 6) == data.y_test.reshape((-1, 1)))
+
+    accuracy = true_predictions / data.x_test.shape[0]
+    print("Accuracy: ", accuracy)
+
+
+if __name__ == "__main__":
+    main()
