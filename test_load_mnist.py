2ac84259eb41c7a2ee5eefbd66ceaad8128a4576
Dimitris Tsirbas <manisfatsa@gmail.com>
2022-12-04 12:33:31 +0200

    Add tests

--- test_load_mnist.py
+++ test_load_mnist.py
@@ -17,6 +17,11 @@ class LoadTest(TestCase):
         assert self.mnist_data.x_test.shape[1] == 784
         assert self.mnist_data.x_valid.shape[1] == 784
 
+    def test_data_contents(self):
+        assert np.max(self.mnist_data.x_train) != np.min(self.mnist_data.x_train)
+        assert np.max(self.mnist_data.x_test) != np.min(self.mnist_data.x_test)
+        assert np.max(self.mnist_data.x_valid) != np.min(self.mnist_data.x_valid)
+
     def test_data_label_eq(self):
         assert self.mnist_data.x_train.shape[0] == self.mnist_data.y_train.shape[0]
         assert self.mnist_data.x_test.shape[0] == self.mnist_data.y_test.shape[0]
@@ -26,6 +31,14 @@ class LoadTest(TestCase):
         assert np.all((self.mnist_data.x_train >= 0) & (self.mnist_data.x_train <= 1))
         assert np.all((self.mnist_data.x_test >= 0) & (self.mnist_data.x_test <= 1))
         assert np.all((self.mnist_data.x_valid >= 0) & (self.mnist_data.x_valid <= 1))
+
+    def test_label_range(self):
+        assert np.min(self.mnist_data.y_train) == 5
+        assert np.max(self.mnist_data.y_train) == 6
+        assert np.min(self.mnist_data.y_test) == 5
+        assert np.max(self.mnist_data.y_test) == 6
+        assert np.min(self.mnist_data.y_valid) == 5
+        assert np.max(self.mnist_data.y_valid) == 6
 
     def test_validation_size(self):
         assert np.isclose(len(self.mnist_data.x_train), len(self.mnist_data.x_valid) * 5, rtol=2)
--- test_logistic_regression.py
+++ test_logistic_regression.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from logistic_regression import LogisticRegClassifier
 import load_mnist
+import numpy as np
 
 
 class LogisticRegClassifierTest(TestCase):
@@ -11,8 +12,17 @@ class LogisticRegClassifierTest(TestCase):
         cls.classifier = LogisticRegClassifier(iters=500, alpha=0.01, lamda=0.1, print_history=False)
 
     def test_train(self):
-        self.classifier.train(self.data.x_train, self.data.y_train)
+        cost_history = self.classifier.train(self.data.x_train, self.data.y_train)
+
+        assert not array_is_same(np.array(cost_history))
+        assert not array_is_same(self.classifier.weights)
 
     def test_test(self):
-        self.classifier.train(self.data.x_test, self.data.y_test)
+        self.classifier.train(self.data.x_train, self.data.y_train)
+        cost_history = self.classifier.train(self.data.x_test, self.data.y_test)
+        assert not array_is_same(np.array(cost_history))
+        assert not array_is_same(self.classifier.weights)
 
+
+def array_is_same(array: np.ndarray) -> bool:
+    return np.all(np.isclose(array, array[0]))
