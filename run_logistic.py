673b14bf4b819dbeafcac69f3f578b3e5c7dd2af
Dimitris Tsirbas <manisfatsa@gmail.com>
2022-12-11 14:15:40 +0200

    Add test/train error plots

--- run_logistic.py
+++ run_logistic.py
@@ -1,25 +1,53 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import os
 from logistic_regression import LogisticRegClassifier
 from load_mnist import load_data
 
-# debug
-# np.seterr(all='raise')
+
+def get_accuracy(predicted_labels: np.ndarray, actual_labels: np.ndarray) -> float:
+    """
+    Get the accuracy of the model based on its predicted and actual labels of its data.
+    :param predicted_labels: the labels which the model predicted
+    :param actual_labels: the actual labels of the data
+    :return: a number between 0 and 1 representing the accuracy of the model
+    """
+    # transform answers back to array with values of 5 and 6 for comparison
+    true_predictions = np.count_nonzero(np.where(predicted_labels == 0, 5, 6) == actual_labels.reshape((-1, 1)))
+    return true_predictions / actual_labels.shape[0]
+
 
 data = load_data()
-classifier = LogisticRegClassifier(iters=2000, alpha=0.01, lamda=0, print_history=True)
+classifier = LogisticRegClassifier(iters=2000, alpha=0.2, lamda=0, print_history=True)
 
-y = np.where(data.y_train == 5, 0, 1)
-cost_history = classifier.train(data.x_train, y)
-# print(cost_history)
+# transform data into binary array for training
+y_train = np.where(data.y_train == 5, 0, 1)
+train_cost_history = classifier.train(data.x_train, y_train)
+
+y_test = np.where(data.y_test == 5, 0, 1)
+test_cost_history = classifier.test(data.x_test, y_test)
 
 # training results
 labels, _ = classifier.predict(data.x_train)
-true_predictions = np.count_nonzero(np.where(labels == 0, 5, 6) == data.y_train.reshape((-1, 1)))
-accuracy = true_predictions / data.x_train.shape[0]
-print("Training accuracy: ", accuracy)
+print("Training accuracy: ", get_accuracy(labels, data.y_train))
 
 # testing results
 labels, _ = classifier.predict(data.x_test)
-true_predictions = np.count_nonzero(np.where(labels == 0, 5, 6) == data.y_test.reshape((-1, 1)))
-accuracy = true_predictions / data.x_test.shape[0]
-print("Testing accuracy: ", accuracy)
+print("Testing accuracy: ", get_accuracy(labels, data.y_test))
+
+fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
+fig.suptitle("Logistic Regression Classifier Error")
+
+ax1.plot(np.arange(0, classifier.iters + 1, 1), train_cost_history, color="red")
+ax1.set_ylabel("Cost")
+ax1.set_xlabel("Number of iterations")
+ax1.set_title("Training")
+
+ax2.plot(np.arange(0, classifier.iters + 1, 1), test_cost_history, color="red")
+ax2.set_ylabel("Cost")
+ax2.set_xlabel("Number of iterations")
+ax2.set_title("Testing")
+
+plt.savefig(os.path.join("images", "logistic_error.png"))
+print("Train/Test error figured saved successfully")
+
