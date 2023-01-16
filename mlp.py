57abae19ac98e1cb3009afd997244f92f77598c2
Dimitris Tsirbas <manisfatsa@gmail.com>
2023-01-16 15:34:30 +0200

    Wrap backprop into train and test methods

--- mlp.py
+++ mlp.py
@@ -41,12 +41,39 @@ class ShallowNetwork:
         self.h_w = np.zeros((input_size, hidden_size))
         self.o_w = np.zeros((hidden_size, output_size))
 
-    def gradient_descent(self, train_data: np.ndarray, train_labels: np.ndarray) -> tuple[int, float, list[float]]:
+    def train(self, train_data: np.ndarray, train_labels: np.ndarray) -> tuple[int, float, list[float]]:
         """
-        Implements the entire gradient descent procedure, updating the model's internal weights and biases.
+        Train the network on a set of training data.
         :param train_data: a numpy array containing the train data
         :param train_labels: a numpy array containing the respective labels for the training data
-        :return: the number of epochs needed to reach the early stopping point, the minimum error found, and the error
+        :return: the number of epochs used, the minimum error found, and the error history
+        """
+        grad, epoch, last_error, error_history = self._gradient_descent(train_data, train_labels)
+        self.h_w = grad["h_w"]
+        self.o_w = grad["o_w"]
+        self.h_b = grad["h_b"]
+        self.o_b = grad["o_b"]
+
+        return epoch, last_error, error_history
+
+    def test(self, test_data: np.ndarray, test_labels: np.ndarray) -> float:
+        """
+        Feed-forward the data into the network to get the computed testing/validation error without
+        updating its weights.
+        :param test_data: a numpy array containing the test data
+        :param test_labels: a numpy array containing the respective labels for the test data
+        :return: the mean test/validation loss
+        """
+        _, epoch, last_error, error_history = self._gradient_descent(test_data, test_labels)
+        return np.array(error_history).mean()
+
+    def _gradient_descent(self, train_data: np.ndarray, train_labels: np.ndarray) \
+            -> tuple[dict[str, np.ndarray], int, float, list[float]]:
+        """
+        Implements the entire gradient descent procedure, calculating the gradient and loss.
+        :param train_data: a numpy array containing the train data
+        :param train_labels: a numpy array containing the respective labels for the training data
+        :return: a dictionary containing the gradient, the number of epochs used, the minimum error found, and the error
         history
         """
         epoch: int = 0  # logging
@@ -55,52 +82,56 @@ class ShallowNetwork:
         best_model_params = None
         error_history = []
 
-        while epochs_since_improvement <= self.patience:
-            dw1, dw2, db1, db2, cost = self.back_propagation(train_data, train_labels)
+        h_w = self.h_w.copy()
+        o_w = self.o_w.copy()
+        h_b = self.h_b.copy()
+        o_b = self.o_b.copy()
 
-            self.h_w -= self.eta * dw1
-            self.o_w -= self.eta * dw2
-            self.h_b -= self.eta * db1
-            self.o_b -= self.eta * db2
+        while epochs_since_improvement <= self.patience:
+            parameters = {"h_w": h_w, "o_w": o_w, "h_b": h_b, "o_b": o_b}
+            dw1, dw2, db1, db2, cost = self._back_propagation(train_data, train_labels, parameters)
+
+            h_w -= self.eta * dw1
+            o_w -= self.eta * dw2
+            h_b -= self.eta * db1
+            o_b -= self.eta * db2
 
             # early stopping
             error = cost.mean()
             if error + self.tolerance < least_error:
-                #print(f"Iteration {epoch} improvement, error {error} least error {least_error}, "
-                      #f"threshold {least_error + self.tolerance}")
+                print(f"Iteration {epoch} improvement from {least_error} to {error}")
                 least_error = error
                 epochs_since_improvement = 0
-                best_model_params = self.h_w, self.o_w, self.h_b, self.o_b
+                best_model_params = parameters
             else:
+                print(f"Iteration {epoch} NO improvement from {least_error} to {error}, "
+                    f"increasing to {epochs_since_improvement}")
                 epochs_since_improvement += 1
-                #print(f"Iteration {epoch} NO improvement, error {error} least error {least_error}, "
-                      #f"threshold {least_error + self.tolerance}")
 
             error_history.append(error)
             epoch += 1
 
-        # keep best model params
-        self.h_w = best_model_params[0]
-        self.o_w = best_model_params[1]
-        self.h_b = best_model_params[2]
-        self.o_b = best_model_params[3]
+        return best_model_params, epoch, least_error, error_history
 
-        return epoch, least_error, error_history
-
-    def back_propagation(self, x: np.ndarray, y: np.ndarray) \
+    def _back_propagation(self, x: np.ndarray, y: np.ndarray, parameters: dict[str, np.ndarray]) \
             -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """
         Implements the back-propagation procedure for one epoch.
+        :param parameters: a dictionary containing the last computed weights and biases
         :param x: a numpy array containing the train data
         :param y: a numpy array containing the respective labels for the training data
         :return: a tuple containing the gradients for the hidden weights, output weights, hidden bias, output bias and
         the current error respectively
         """
+        h_w = parameters["h_w"]
+        o_w = parameters["o_w"]
+        h_b = parameters["h_b"]
+        o_b = parameters["o_b"]
 
         # Forward pass
-        z1 = x.dot(self.h_w) + self.h_b
+        z1 = x.dot(h_w) + h_b
         a1 = self.activation_func(z1)
-        z2 = a1.dot(self.o_w) + self.o_b
+        z2 = a1.dot(o_w) + o_b
         a2 = self.activation_func(z2)
 
         # Backward pass
@@ -114,7 +145,7 @@ class ShallowNetwork:
         db2: np.ndarray = (1 / m) * np.sum(dz2, axis=0)
 
         # hidden layer activation derivative
-        da1 = dz2.dot(self.o_w.T)
+        da1 = dz2.dot(o_w.T)
         dz1 = da1 * self.activation_func_prime(z1)
         dw1: np.ndarray = (1 / m) * x.T.dot(dz1)
         db1: np.ndarray = (1 / m) * np.sum(dz1, axis=0)
@@ -140,3 +171,4 @@ class ShallowNetwork:
         :return: a numpy array containing a binary classification for each data point
         """
         return np.where(self.output(test_data) < 0.5, 0, 1)
+
